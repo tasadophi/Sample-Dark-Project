bc97aa03f249adda8fdc4641f6e4e518e3a502c8
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-06-18 15:11:07 +0430

    get navItems and menuItems object added

---
+++ src/utils/getMenuItems.js
@@ -0,0 +1,17 @@
+import { MdCampaign } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+
+const menuItems = [
+  {
+    id: 1,
+    name: "Campaigns",
+    icon: <MdCampaign />,
+  },
+  {
+    id: 2,
+    name: "Settings",
+    icon: <FiSettings />,
+  },
+];
+
+export default menuItems;
---
+++ src/utils/getNavItems.js
@@ -0,0 +1,27 @@
+import { FaGoogleDrive } from "react-icons/fa";
+import { FaHubspot } from "react-icons/fa";
+import { TbLetterP } from "react-icons/tb";
+
+const navItems = [
+  {
+    id: 1,
+    name: "Google Drive",
+    icon: <FaGoogleDrive />,
+  },
+  {
+    id: 2,
+    name: "Hubspot",
+    icon: <FaHubspot />,
+  },
+  {
+    id: 3,
+    name: "Pipedrive",
+    icon: (
+      <span className="bg-gray-300 p-1 rounded w-full h-full block">
+        <TbLetterP />
+      </span>
+    ),
+  },
+];
+
+export default navItems;
