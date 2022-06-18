ffb66367156ffbd4f7459a711e3285bbc5e74578
Ali Mammoudi <alimahmoodi3828@gmail.com>
2022-06-18 16:51:08 +0430

    searchBar completed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,22 @@
+import React from "react";
 import { FaSearch } from "react-icons/fa";
-const SearchBar = () => {
+const SearchBar = ({ setContent, setContentColor, setNavSelected }) => {
+  const changeHandler = (e) => {
+    setContent(e.target.value);
+    setNavSelected("settings");
+    setContentColor("bg-blue-500");
+  };
   return (
-    <div className="bg-white flex items-center border border-gray-200 shadow-md w-full">
+    <div className="bg-white flex items-center rounded-md border border-gray-200 shadow-md w-full">
       <span className="p-2">{<FaSearch />}</span>
       <input
         type="text"
         className="outline-none border-none p-2 w-full"
         placeholder="Search"
+        onChange={changeHandler}
       />
     </div>
   );
 };
 
-export default SearchBar;
+export default React.memo(SearchBar);
